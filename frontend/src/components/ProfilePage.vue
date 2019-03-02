588685438cfbb42b87ac3e18f316f86df51d2a9d
Rushil Rushil <rdaya@isaziconsulting.co.za>
2019-03-02 02:42:41 +0100

    added svg profile pic

---
+++ frontend/src/components/ProfilePage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div id="img-holder">
+            <img src="../assets/man-profile-svgrepo-com.svg" /> 
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'ProfilePage',
+    data(){
+        return{
+            name:'',
+            company:'',
+            age:'',
+            gender:''
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+    #img-holder{
+        margin:30px;
+        max-width:400px;
+        text-align:center;
+    }
+</style>
