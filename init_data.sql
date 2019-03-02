29cb3316054d10f65360ad868fe6ff8a799f2be0
Eric Massip <ericmassip1@gmail.com>
2019-03-02 02:45:16 +0100

    Added de lijn and nmbs logo imgs

--- init_data.sql
+++ init_data.sql
@@ -38,8 +38,8 @@ INSERT INTO route (user_id,route_date,total_km) VALUES (2,'2019-01-07',4);
 INSERT INTO route (user_id,route_date,total_km) VALUES (3,'2019-01-07',3);
 INSERT INTO route (user_id,route_date,total_km) VALUES (4,'2019-01-07',2);
 
-INSERT INTO discount (name,url,img,discount_rate) VALUES ('NMBS','www.belgiantrain.be','https://image.advance.net/home/advance-media/width460/img/newyorkupstatecom_national_desk_blog/photo/2018/03/15/uberjpg-5f9a611fad597f9c.jpg',10);
-INSERT INTO discount (name,url,img,discount_rate) VALUES ('De Lijn','www.delijn.be','https://image.advance.net/home/advance-media/width460/img/newyorkupstatecom_national_desk_blog/photo/2018/03/15/uberjpg-5f9a611fad597f9c.jpg',12);
+INSERT INTO discount (name,url,img,discount_rate) VALUES ('NMBS','www.belgiantrain.be','https://seeklogo.com/images/N/NMBS_-_SNCB-logo-5E5F5EA38E-seeklogo.com.png',10);
+INSERT INTO discount (name,url,img,discount_rate) VALUES ('De Lijn','www.delijn.be','https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/De_Lijn.svg/266px-De_Lijn.svg.png',12);
 INSERT INTO discount (name,url,img,discount_rate) VALUES ('UBER','www.uber.com','https://image.advance.net/home/advance-media/width460/img/newyorkupstatecom_national_desk_blog/photo/2018/03/15/uberjpg-5f9a611fad597f9c.jpg',5);
 INSERT INTO discount (name,url,img,discount_rate) VALUES ('VELO','www.velo.be','https://www.velo.be/sites/all/themes/velo/images/logo_velo_wit.png',15);
 INSERT INTO discount (name,url,img,discount_rate) VALUES ('BLABLACAR','www.blablacar.com','https://www.vostoknewventures.com/wp-content/uploads/2015/10/bla_bla_car_logo.png',7.5);
