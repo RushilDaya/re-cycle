ee3c06c54f5ac9c78eb99cb06c18ac35e4e59609
Eric Massip <ericmassip1@gmail.com>
2019-03-02 15:55:30 +0100

    Heroku change

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, jsonify
 #from flask_cors import CORS
 
 app = Flask(__name__,
-            static_folder="dist/static",
             template_folder="dist")
 #CORS(app)
 
