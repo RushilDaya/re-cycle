ffdb0b1a62a55db37461b2c6ebe24d8b09676806
IgnacioGarridoBotella <ignaciogabo95@gmail.com>
2019-03-02 08:36:33 +0100

    Discounts conneected to db

--- frontend/src/components/DiscountsPage.vue
+++ frontend/src/components/DiscountsPage.vue
@@ -4,7 +4,8 @@
 <table style="width:100%" class="table">
 	<thead class="thead-dark">
 		<th scope="col"></th>
-      	<th scope="col"> Discount </th>
+      	<th scope="col"> <center> Discount </center>
+		</th>
     </thead>
  	<tbody>
 		  <tr v-for="dis in discount">
@@ -15,7 +16,7 @@
 		    </td>
 		    <td style="text-align: center; vertical-align: middle;">
 		    	<strong>
-		    		<a v-bind:href="dis.url" style="font-size: 20px; color: green" ><center> {{ dis.userdiscount }}% </center> </a>
+		    		<a :href="dis.url" style="font-size: 20px; color: green" ><center> {{ dis.userdiscount }}% </center> </a>
 		    	</strong>
 			</td>
 		  </tr>
@@ -36,22 +37,17 @@ export default {
     name:'DiscountsPage',
     data(){
         return{
-        	discount:[
-	        	{name: 'blablacar',
-	        	userdiscount: 21.23,
-	        	img: 'https://blog.blablacar.es/wp-content/uploads/2018/01/BBC_horizontal_positive_1002x274%20(1).png',
-	        	url: 'https://www.blablacar.com/'},
-	        	{name: 'uber',
-	        	userdiscount: 23.45,
-	        	img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSxCihfF8hj92MbylUESP6yfSAOrH8nK57V2S_1FcCYh6YGyJE',
-	        	url: 'https://www.uber.com/be/en/'},
-	        	{name: 'velo',
-	        	userdiscount: 17.89,
-	        	img: 'https://www.velo.be/sites/all/themes/velo/images/transparant.png',
-	        	url: 'https://www.velo.be/nl'}
-        	]
+        	discount:[]
         }
+    },
+    mounted(){
+        this.$http.get(this.FLASK_URL+'/api/discount/'+this.LOGGED_IN_USER)
+            .then(response => {
+                this.discount = response.data
+            })        
     }
+
+
 
 }
 </script>
