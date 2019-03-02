19a929b0dbc7d073ddef038747bce2da7fbfe274
Murilo Cunha <murilo.kuniyoshi@hotmail.com>
2019-03-02 11:45:28 +0100

    doing sorting

--- frontend/src/components/SocialPage.vue
+++ frontend/src/components/SocialPage.vue
@@ -5,9 +5,9 @@
         <b-card no-body>
             <b-tabs pills card>
                 <b-tab title="Individual" active>
-                    <table class="table table-hover">
+                    <table class="table">
                             <thead>
-                                <tr><th>#</th><th>Name</th><th>CO2 Prevented</th><th>Company</th><th>City</th></tr>
+                                <tr><th>#</th><th>Name</th><th>CO2 Prevented</th></tr>
                             </thead>
                             <tbody>
                                 <tr v-for="(row,index) in rows" :key="index">
@@ -20,20 +20,14 @@
                                     <td>
                                         {{row.co2}}
                                     </td>
-                                    <td>
-                                        {{row.company}}
-                                    </td>
-                                    <td>
-                                        {{row.city}}
-                                    </td>
                                 </tr>
                             </tbody>
                         </table>
                 </b-tab>
                 <b-tab title="Company">
-                    <table class="table table-hover">
+                    <table class="table">
                             <thead>
-                                <tr><th>#</th><th>Company</th><th class>CO2 Prevented</th><th>City</th></tr>
+                                <tr><th>#</th><th>Company</th><th class>CO2 Prevented</th></tr>
                             </thead>
                             <tbody>
                                 <tr v-for="(row, index) in rows"  :key="index">
@@ -46,15 +40,12 @@
                                     <td>
                                         {{row.co2}}
                                     </td>
-                                    <td>
-                                        {{row.city}}
-                                    </td>
                                 </tr>
                         </tbody>
                     </table>
                 </b-tab>
                 <b-tab title="City">
-                    <table class="table table-hover">
+                    <table class="table">
                             <thead>
                                 <tr><th>#</th><th>City</th><th>CO2 Prevented</th></tr>
                             </thead>
@@ -93,29 +84,20 @@
         var rows = [
                 { name: "Murilo Cunha", co2: 100000, company: 'Brazil', city: "Sao Paulo" },
                 { name: "Rushil Daya", co2: 10000, company: 'Mothaland', city: "Johaa" },
-                { name: "Massi Picano", co2: 1000, company: 'Cataluna', city: "Barcelona" },
+                { name: "Massi Picano", co2: 1000000, company: 'Cataluna', city: "Barcelona" },
                 { name: "Nachito", co2: 100, company: 'The enemy', city: "Merengue" },
                 { name: "Mr. Rocketo", co2: 10, company: 'Cern', city: "Hydron Colider" },
-                ]
-        this.rows = this.sort(rows)
-    },
-    methods:{
-        sort(rows) {
-            return rows
-        }
+                ];
 
+                // Sorting, STILL NEED TO INTEGRATE WITH FLASK
+        this.rows.sort(function(a,b) {
+            return parseFloat(a.co2) - parseFloat(b.co2);
+        });
+        this.rows
     }
     }
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
-h1 {
-  font-family: 'Montserrat', sans-serif;
-  display: grid;
-  grid-template-rows: auto;
-  justify-items: center;
-  align-items: 100%;
-}
+
 </style>
-<!--<li v-for="(name, co2, city) in item":key="index">{{name.item, co2.item, city.item}}
